aacefab2d03f67aa1876a39c490a386326d990f0
Lokeshgajji <132226574+Lokeshgajji@users.noreply.github.com>
2024-01-11 18:48:03 -0500

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "example" {
     ami           = "ami-0c7217cdde317cfec"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 subnet_id = "subnet-0986ca0ae3f6dbb9b"
-key_name = "ubuntunodejs.pem"
+key_name = "ubuntunodejs"
 }

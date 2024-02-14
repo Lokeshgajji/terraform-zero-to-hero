11754642785b92782911caeb7b43be1efa434eda
Lokeshgajji <132226574+Lokeshgajji@users.noreply.github.com>
2024-02-14 12:03:22 -0500

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,10 +1,23 @@
+# Configure the AWS provider
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1"  # Specify your desired region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c7217cdde317cfec"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
-subnet_id = "subnet-0986ca0ae3f6dbb9b"
-key_name = "ubuntunodejs"
+# Define a data source to fetch information about the EC2 instance
+data "aws_instances" "existing_instances" {
+  instance_tags {
+    Name = "aws_instance.example"  # Specify the tag of your EC2 instance
+  }
 }
+
+# Remove the EC2 instance from the Terraform state
+resource "aws_instance" "existing_instances" {
+  for_each = data.aws_instances.existing_instances.ids
+
+  # This is a dummy resource that only exists to trigger state removal
+  # It does not perform any actions on the EC2 instance
+  provisioner "local-exec" {
+    command = "echo 'This resource is only used for state removal'"
+  }
+}
+
